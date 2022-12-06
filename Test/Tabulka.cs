43066eff209256965f9aba41c9a3381b386ff40b
Lex-ion <killkiisdog@gmail.com>
2022-12-06 15:39:11 +0100

    Přidejte soubory projektu.

---
+++ Test/Tabulka.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Tabulka
+    {
+
+        public static int[] kod =
+        {
+            100,
+            200,
+            300,
+            400,
+            500,
+            600
+
+        };
+
+        public static string[] nazev =
+        {
+
+            "informace",
+            "úspěch",
+            "přesměrování",
+            "chyba klienta",
+            "chyba serveru"
+            
+
+        };
+
+
+
+    }
+}
